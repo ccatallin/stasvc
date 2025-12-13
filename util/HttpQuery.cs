fefc1cd05260d811cfd3dd0a73b3fd9b9ff5abce
Catalin Calugaroiu <catalin.calugaroiu@outlook.com>
2025-12-13 21:41:14 +0200

    optimized HttpQuery class; add method GetValueAsync

--- util/HttpQuery.cs
+++ util/HttpQuery.cs
@@ -1,52 +1,83 @@
-﻿﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿﻿using System.Net;
 
 namespace cc.net
 {
-
-public class HttpQuery
-{
-	private HttpClient httpClient = null!;
-
-    public HttpQuery()
+    public class HttpQuery
     {
-        this.httpClient = new HttpClient();
-    }
+        private HttpClient httpClient = null!;
 
-    public HttpQuery(string uriBaseAddress)
-    {
-        this.httpClient = new()
+        public HttpQuery()
         {
-            BaseAddress = new Uri(uriBaseAddress),
-        };
-    }
-
-    public string BaseAddress 
-    { 
-        get
-        {
-            return this.httpClient.BaseAddress?.ToString() ?? string.Empty;
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            };
+            this.httpClient = new HttpClient(handler);
         }
 
-        set 
+        public HttpQuery(string uriBaseAddress)
         {
-            this.httpClient.BaseAddress = new Uri(value.Trim());
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            };
+            this.httpClient = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(uriBaseAddress),
+            };
         }
-    }
 
-	public async Task<string> GetAsync(string queryString)
-	{
-        using HttpResponseMessage response = await this.httpClient.GetAsync($"{queryString.Trim()}");
-        response.EnsureSuccessStatusCode();
-        return await response.Content.ReadAsStringAsync();
-    }
+        public string BaseAddress 
+        { 
+            get
+            {
+                return this.httpClient.BaseAddress?.ToString() ?? string.Empty;
+            }
 
-	public async Task<string> GetStringAsync(string queryString)
-	{
-        return await this.httpClient.GetStringAsync($"{queryString.Trim()}");
-    }
+            set 
+            {
+                this.httpClient.BaseAddress = new Uri(value.Trim());
+            }
+        }
 
-} /* end class HttpQuery */
+        public async Task<string> GetAsync(string queryString)
+        {
+            using HttpResponseMessage response = await this.httpClient.GetAsync($"{queryString.Trim()}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
 
+        public async Task<string> GetStringAsync(string queryString)
+        {
+            return await this.httpClient.GetStringAsync($"{queryString.Trim()}");
+        }
+
+        public async Task<string> GetValueAsync(string queryString, string startLabel, string endLabel)
+        {
+            try
+            {
+                var bodyResponse = await this.GetStringAsync(queryString);
+                
+                int startTagIndex = bodyResponse.IndexOf(startLabel);
+                if (startTagIndex == -1)
+                {
+                    return string.Empty;
+                }
+
+                int startIndex = startTagIndex + startLabel.Length;
+                int endIndex = bodyResponse.IndexOf(endLabel, startIndex);
+
+                if (endIndex == -1)
+                {
+                    return string.Empty;
+                }
+
+                return bodyResponse.Substring(startIndex, endIndex - startIndex).Trim();   
+            }
+            catch (HttpRequestException)
+            {
+                return string.Empty;
+            }
+        }
+    } /* end class HttpQuery */
 } /* end cc.net namespace */
